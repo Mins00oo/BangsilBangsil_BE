429fa9dfbee540c476dd7dcad49aee49baac3e03
mins00oo <alstn7223@gmail.com>
2023-02-13 14:45:13 +0900

    초기 세팅

---
+++ src/main/java/com/bangsil/bangsil/BangsilApplication.java
@@ -0,0 +1,13 @@
+package com.bangsil.bangsil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BangsilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BangsilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bangsil/bangsil/BangsilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bangsil.bangsil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BangsilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
