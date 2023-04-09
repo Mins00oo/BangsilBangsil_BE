3af0c05e2eea36195b0a3b64a640350c33ca688e
mins00oo <alstn7223@gmail.com>
2023-04-09 15:12:17 +0900

    FEAT: jasypt 적용해서 application.yml에 민감정보들은 암호화해서 업로드
    속성 추가한 후, yml파일에 개인민감정보들 암호화해서 ENC로 감싸주어서 푸쉬
    
    해결: #28

---
+++ src/test/java/com/bangsil/bangsil/config/JasyptConfigTest.java
@@ -0,0 +1,44 @@
+package com.bangsil.bangsil.config;
+
+import org.assertj.core.api.Assertions;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class JasyptConfigTest {
+
+    @Test
+    void jasypt(){
+        String url = "jdbc:mysql://localhost:3306/bangsil_user";
+        String username = "bangsilbangsil";
+        String password = "1234";
+
+        String encryptUrl = jasyptEncrypt(url);
+        String encryptUsername = jasyptEncrypt(username);
+        String encryptPassword = jasyptEncrypt(password);
+
+        System.out.println("encryptUrl:" + encryptUrl);
+        System.out.println("encryptUsername:" + encryptUsername);
+        System.out.println("encryptPassword:" + encryptPassword);
+
+        Assertions.assertThat(url).isEqualTo(jasyptDecryt(encryptUrl));
+    }
+
+    private String jasyptEncrypt(String input) {
+        String key = "1234";
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setAlgorithm("PBEWithMD5AndDES");
+        encryptor.setPassword(key);
+        return encryptor.encrypt(input);
+    }
+
+    private String jasyptDecryt(String input){
+        String key = "1234";
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setAlgorithm("PBEWithMD5AndDES");
+        encryptor.setPassword(key);
+        return encryptor.decrypt(input);
+    }
+}
